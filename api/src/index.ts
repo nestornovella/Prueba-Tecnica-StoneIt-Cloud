99ce0cbdfd750f682d50f59bda1f2b04dc4b6ce6
Nestor <importarnorth@gmail.com>
2024-11-19 17:30:03 -0500

    creando cron para enviar email a los usuarios cuando la tarea este proxima a terminar
    Signed-off-by: Nestor <importarnorth@gmail.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
+import cron from 'node-cron';
 import express,{ NextFunction, Request, Response } from "express"
 import cors from "cors"
 import morgan from 'morgan'
 import router from './routes/index.routes'
 import { captureRes } from "./helpers/capturRes"
+import { checkTask } from './helpers/reminderMail';
 export interface Error{
     message: string,
     status: number
@@ -20,6 +22,13 @@ app.use(cors({
 app.use(morgan('dev'))
 app.use(captureRes)
 app.use('/', router)
+
+export function startTaskReminderService() {
+    cron.schedule('0 12 * * *', async () => {
+      console.log('Running task reminder check...');
+      await checkTask();
+    });
+  }
 
 app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
     const status = err.status || 500;  
