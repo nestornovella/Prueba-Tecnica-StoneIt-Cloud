0a9c9d2197ae02bbbafbaa94954270fc0a5b2f55
Nestor <importarnorth@gmail.com>
2024-11-19 11:17:38 -0500

    middleware para validacion de email y contraseña segura
    Signed-off-by: Nestor <importarnorth@gmail.com>

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 import { getAllUsers, userRegister } from '../controllers/user.controller'
+import { validateUserInput } from '../middleware/validateUserInput'
 
 const router = Router()
 
 
 router.get('/', getAllUsers)
-router.post('/', userRegister)
+router.post('/', validateUserInput, userRegister)
 
 
 
